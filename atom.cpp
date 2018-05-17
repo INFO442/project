e2a606471db97d659f0b111ebc22a6805c958f0e
YiranCHEN1994 <113160012@qq.com>
2018-05-17 21:57:49 +0200

    atom

---
+++ atom.cpp
@@ -0,0 +1,34 @@
+/*
+ * atom.cpp
+ *
+ *  Created on: May 17, 2018
+ *      Author: yiranchen
+ */
+#include <iostream>
+#include <vector>
+#include "atom.h"
+using namespace std;
+
+Atom::Atom(vector<int>& line): line(line),arity(line.size())
+{ }
+vector<int> Atom::get_all() const{
+	return this->line;
+}
+
+int Atom::get(int index) const{
+	if(index >=this->arity){
+		cout<<"error:index>=arity"<<endl;
+		return -1;
+	}
+	return this->line[index];
+}
+int Atom::get_arity() const{
+	return this->arity;
+}
+void Atom::print_line() const{
+		for(vector<int>::const_iterator itr=this->line.begin();itr!=this->line.end();++itr){
+			printf(" %d ",*itr);
+		}
+		printf("\n");
+}
+
---
+++ atom.h
@@ -0,0 +1,35 @@
+/*
+ * atom.h
+ *
+ *  Created on: May 17, 2018
+ *      Author: yiranchen
+ */
+
+#ifndef ATOM_H_
+#define ATOM_H_
+
+#include <iostream>
+#include <vector>
+using namespace std;
+class Atom{
+public:
+	//constructor from vector<int>
+	Atom(vector<int>& line);
+	//return all content
+	vector<int> get_all() const;
+	//return the element at index
+	int get(int index)const;
+	//get arity
+	int get_arity() const;
+	//print all content
+	void print_line() const;
+private:
+	vector<int> line;
+	const int arity;
+};
+
+
+
+
+
+#endif /* ATOM_H_ */
